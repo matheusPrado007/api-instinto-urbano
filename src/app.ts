4f463a6441ef809a5328f699f1ca411d0479876f
Prado <peu_prado01@hotmail.com>
2023-12-16 03:02:50 -0300

    fix: atualiza cors

--- src/app.ts
+++ src/app.ts
@@ -9,11 +9,14 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-app.use(cors({ origin: '*' }));
-const corsOptions = {
-  origin: ['http://localhost:3000', 'http://192.168.2.121:3000'],
-};
-app.use(cors(corsOptions));
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000/');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', true as any);
+  next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
